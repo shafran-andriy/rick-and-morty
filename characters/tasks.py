a41b8539208e44f35002529f3ba18b5fa6d76807
Andriy Shafran <shafran.andriy@gmail.com>
2026-04-26 20:31:57 +0300

    feat: added task for update db from API

--- characters/tasks.py
+++ characters/tasks.py
@@ -1,7 +1,7 @@
-from characters.models import Character
+from characters.scraper import sync_characters_with_api
 from celery import shared_task
 
 
 @shared_task
-def count_characters() -> int:
-    return Character.objects.count()
+def run_sync_with_api() -> None:
+    sync_characters_with_api()
